ba89084dd9b33f26f5a68c23952029ce9d5b8b86
Andy Martin <andrew.martin@springernature.com>
2016-11-11 18:51:42 +0000

    tells the time in a better format

--- GreyMatter/tell_time.py
+++ GreyMatter/tell_time.py
@@ -2,4 +2,4 @@ from datetime import datetime
 from SenseCells.tts import tts
 
 def what_is_time():
-    tts("The time is " + datetime.strftime(datetime.now(), '%H::%M:%S'))
+    tts("The time is " + datetime.strftime(datetime.now(), '%H:%M'))
