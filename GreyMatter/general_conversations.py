dc12b40b7aeb15008244111b2b8568661c7bedb2
andrewmartin <andrew.martin@springernature.com>
2016-11-14 11:22:30 +0000

    Adds jokes and timing

--- GreyMatter/general_conversations.py
+++ GreyMatter/general_conversations.py
@@ -2,15 +2,23 @@ import random
 from SenseCells.tts import tts
 
 def who_are_you():
-    messages = ['I am Melissa, your lovely personal assistant.', 'Melissa', 'I am Melissa', 'My name is Melissa', 'Who am I? Who are you?', 'You can call me Melissa']
+    messages = ['I am Melissa, your personal assistant.',
+                'Melissa', 'I am Melissa',
+                'My name is Melissa',
+                'Who am I? [[slnc 500]] Who are you?',
+                'You can call me Melissa']
     tts(random.choice(messages))
 
 def tell_joke():
-    jokes = ['What happened to the frogs car when it broke down?... It got toad away', 'Why was six scared of seven? Because seven ate nine', 'I might kill you in your sleep... only kidding']
+    jokes = ['Today, a man knocked on my door and asked for a small donation towards the local swimming pool. [[slnc 1500]] I gave him a glass of water.',
+             'What happened to the frogs car when it broke down? [[slnc 3000]] It got toad away',
+             'Why was six scared of seven? [[slnc 3000]] Because seven ate nine',
+             'I might kill you in your sleep [[slnc 2000]] only kidding',
+             'Knock Knock. [[slnc 1500]] The interrupting sheep. [[slnc 700]] [[inpt PHON]] Ba r r r r r r r r r r!']
     tts(random.choice(jokes))
 
 def who_am_i(name):
-    tts('You are ' + name + ', a fledgling programmer, who is not very good')
+    tts('You are ' + name)
 
 def where_born():
     tts('I was created by Andy Martin, in his bungalow in Orpington')
